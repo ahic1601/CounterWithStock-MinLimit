14bd182f629bae9fc9c824f2db5915d22b7c3f69
ahic1601 <113142689+ahic1601@users.noreply.github.com>
2023-02-13 17:34:42 -0300

    1 file modified

--- src/Component1.tsx
+++ src/Component1.tsx
@@ -4,31 +4,48 @@ import { useState, useEffect } from 'react';
 const Contador = ({ stockDisponible }) => {
   const [numero, setNumero] = useState(0);
   const contadorMin = 0;
+  const [sumarIcon,setSumarIcon] = useState("+");
+  const [restarIcon,setRestarIcon] = useState("-");
   const sumar = () => {
     setNumero(numero + 1);
-    numero >= stockDisponible ?
-    setNumero(stockDisponible): sumar
+    if (numero < stockDisponible) {
+      sumar;
+      setRestarIcon("-")
+    }else {
+      setNumero (stockDisponible);
+      setSumarIcon ("Max");
+    }
   };
+
   const reset = () => {
     setNumero(0);
+    setSumarIcon("+")
+    setRestarIcon("-")
   };
+
   const restar = () => {
     setNumero(numero - 1);
-    numero <= contadorMin ? 
-    setNumero(contadorMin) : setNumero(numero - 1);
+    if (numero <= contadorMin){
+      setNumero(contadorMin);
+      setRestarIcon("Min")
+      }else{
+      setNumero(numero - 1);
+      setSumarIcon("+")
+      setRestarIcon("-")
+    }
   };
 
   return (
     <div>
       <p>{numero}</p>
       <button id="btn_sumar" onClick={sumar}>
-        +
+        {sumarIcon}
       </button>
       <button id="btn_reset" onClick={reset}>
         Reset
       </button>
       <button id="btn_restar" onClick={restar}>
-        -
+        {restarIcon}
       </button>
     </div>
   );
